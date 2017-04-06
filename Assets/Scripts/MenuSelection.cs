4bcfb59faf1049c7a6dbf01e59fe97ab8716736a
Mike Morrone <Hanegwen725@gmail.com>
2017-04-05 20:15:31 -0500

    Test

---
+++ Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void One()
+    {
+        SceneManager.LoadScene("1");
+    }
+
+    public void Two()
+    {
+        SceneManager.LoadScene("2");
+    }
+
+    public void Three()
+    {
+        SceneManager.LoadScene("3");
+    }
+}
